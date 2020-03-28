fb334a07d15246c962d5417a54fdedd35f2cec63
Gabe Hargrave <gkhargrave@gmail.com>
2020-03-28 13:19:25 -0700

    Remove common in favor of velcrostrip

--- src/components/authors/containers/AuthorsList/AuthorsList.tsx
+++ src/components/authors/containers/AuthorsList/AuthorsList.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react'
 import { RouteComponentProps } from '@reach/router'
+import { Alert } from '@gargrave/velcrostrip/lib/Alert'
 
 export type AuthorsListProps = {} & RouteComponentProps
 
 export const AuthorsList: React.FC<AuthorsListProps> = React.memo(() => {
-  return <div>Hello, AuthorsList!</div>
+  return (
+    <>
+      <div>Hello, AuthorsList!</div>
+      <Alert>Test</Alert>
+    </>
+  )
 })
--- src/components/common/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './Alert'
-export * from './Button'
-export * from './Loader'
-export * from './styles'
