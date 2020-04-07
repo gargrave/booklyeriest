1f785c8144167a74a5969d1f117e32483f257cf7
Gabe Hargrave <gkhargrave@gmail.com>
2020-04-07 10:26:20 -0700

    Deps upgrades and test request

--- src/components/authors/containers/AuthorsList/AuthorsList.tsx
+++ src/components/authors/containers/AuthorsList/AuthorsList.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react'
 import { RouteComponentProps } from '@reach/router'
+
 import { Alert } from '@gargrave/velcrostrip/lib/Alert'
 
 export type AuthorsListProps = {} & RouteComponentProps
 
 export const AuthorsList: React.FC<AuthorsListProps> = React.memo(() => {
+  React.useEffect(() => {
+    fetch(
+      `http://localhost:8080/api/v1/books?include=author&fields[book]=title,author&fields[author]=firstName,lastName`,
+    )
+  }, [])
+
   return (
     <>
       <div>Hello, AuthorsList!</div>
