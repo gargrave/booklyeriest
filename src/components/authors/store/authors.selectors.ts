d8c0243c1a50646c6062fb8d26d27f01346162bb
Gabe Hargrave <gkhargrave@gmail.com>
2020-04-16 10:24:34 -0700

    Sort authors by last name

--- src/components/authors/store/authors.selectors.ts
+++ src/components/authors/store/authors.selectors.ts
@@ -1,9 +1,15 @@
 import { createSelector } from '@reduxjs/toolkit'
-import * as R from 'ramda'
 
 import { AppState, KeyObjectMap } from 'store'
 import { Author, ReduxAuthor } from './authors.types'
 import { hydrateAuthor } from './authors.utils'
+
+const sortByLastName = (a: Author, b: Author) => {
+  const lnA = a.lastName.toLowerCase()
+  const lnB = b.lastName.toLowerCase()
+
+  return lnA === lnB ? 0 : lnA > lnB ? 1 : -1
+}
 
 const getAuthorsData = (state: AppState): KeyObjectMap<ReduxAuthor> =>
   state.authors.data
@@ -11,8 +17,11 @@ const getAuthorsData = (state: AppState): KeyObjectMap<ReduxAuthor> =>
 export const getAuthors = createSelector(
   getAuthorsData,
   (authors): Author[] => {
-    return R.map(hydrateAuthor, R.values(authors))
+    return Object.values(authors)
+      .map(hydrateAuthor)
+      .sort(sortByLastName)
   },
 )
 
-export const getAuthorsRequestPending = (state) => state.authors.requestPending
+export const getAuthorsRequestPending = (state: AppState) =>
+  state.authors.requestPending
